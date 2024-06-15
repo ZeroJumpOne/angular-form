04a56284f19b51885bf7d065651747d334f598f9
M.Ing. Isaac Hdez Castro <isaac@zerojumpone.mx>
2024-06-14 21:42:36 -0600

    form-reactive componentes switch radiobutton, checkbutton

--- src/app/reactive/pages/frm-switches/frm-switches.component.ts
+++ src/app/reactive/pages/frm-switches/frm-switches.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-frm-switches',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './frm-switches.component.css'
 })
 export class FrmSwitchesComponent {
+
+   public frmSwitches: FormGroup = this.fb.group({
+      gender: ['M', Validators.required],
+      wantNotificacions: [ true, Validators.required ],
+      termsAndConditions: [ false, Validators.requiredTrue ]
+   });
+
+   public person = {
+      gender: 'F',
+      wantNofications: false
+   }
+
+   constructor(
+      private fb: FormBuilder,
+   ) {}
+
+   isValidField(field: string) {
+      return this.frmSwitches.controls[field].getError('required') &&
+         this.frmSwitches.controls[field].touched;
+   }
+
+   public onSave(): void {
+
+      if (this.frmSwitches.invalid) {
+         this.frmSwitches.markAllAsTouched();
+         return; /* No continua con el siguiente codigo de aceptacion */
+      }
+
+      const { termsAndConditions, ...newPerson } = this.frmSwitches.value;
+
+      this.person = newPerson;
+      console.log(this.frmSwitches.value);
+      console.log(this.person);
+   }
 
 }
